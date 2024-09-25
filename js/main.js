66710b6929b7551def76e55d8bf067e45ba13d49
Nnaji Christian <54823349+chinemerem123@users.noreply.github.com>
2024-09-25 23:50:35 +0100

    updaate am

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ const text =
             }
         }
         setInterval(writeText, 50);
+
+
+
+        function myFunction() {
+            // Get the text field
+            var copyText = document.getElementById("myInput");
+          
+            // Select the text field
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); // For mobile devices
+          
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(copyText.value);
+          }
